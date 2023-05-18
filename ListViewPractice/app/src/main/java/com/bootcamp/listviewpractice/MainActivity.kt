d322efee633eeda60ae408b377a3b45e4f88b616
Barbara Carvajal <barb.carvajalsaez@gmail.com>
2023-05-18 19:52:14 -0400

    aa

--- ListViewPractice/app/src/main/java/com/bootcamp/listviewpractice/MainActivity.kt
+++ ListViewPractice/app/src/main/java/com/bootcamp/listviewpractice/MainActivity.kt
@@ -2,10 +2,21 @@ package com.bootcamp.listviewpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bootcamp.listviewpractice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var listAdapter: ListAdapter
+    private lateinit var listData: ListData
+    var dataArrayList = ArrayList<ListData?>()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val nameList = arrayOf("Pasta")
     }
 }
